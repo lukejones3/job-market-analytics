57b60db0014d54ade4deaf287afc13b4f9911e01
Luke Jones <jones31luke@gmail.com>
2026-04-05 01:23:18 -0500

    update

--- dbt/job_analytics_dbt/models/marts/core/dim_companies.sql
+++ dbt/job_analytics_dbt/models/marts/core/dim_companies.sql
@@ -1,5 +1,8 @@
 select
-  company_id,
-  company_name,
-  company_type
-from {{ ref('stg_companies') }}
+  c.company_id,
+  c.company_name,
+  c.company_type,
+  dc.sector
+from {{ ref('stg_companies') }} c
+left join {{ source('job_analytics', 'discovered_companies') }} dc
+  on lower(c.company_name) = lower(dc.company_name)
