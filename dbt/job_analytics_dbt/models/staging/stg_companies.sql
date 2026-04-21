eb9396503392c12d994c08b0b97cd5cf2d3756a3
Luke Jones <jones31luke@gmail.com>
2026-04-20 23:58:12 -0500

    fix: add sector to stg_companies, fix dim_companies intensity join

--- dbt/job_analytics_dbt/models/staging/stg_companies.sql
+++ dbt/job_analytics_dbt/models/staging/stg_companies.sql
@@ -1,5 +1,6 @@
 select
   company_id,
   nullif(trim(company_name), '') as company_name,
-  nullif(trim(company_type), '') as company_type
+  nullif(trim(company_type), '') as company_type,
+  nullif(trim(sector), '') as sector
 from {{ source('job_analytics','companies') }}
